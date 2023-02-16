8816c3a66c074cdf8a53259aa2d9597767bd9cb1
MansiRavindraMohan <manasimohan29@gmail.com>
2023-02-16 18:32:21 +0530

    Calculate length of line

--- Program.cs
+++ Program.cs
@@ -2,11 +2,37 @@
 {
     class Program
     {
-        public static void Main(string[] args)
+        public void LineofLength()
         {
-            Console.WriteLine("Welcome to Line Comparison Problem using OOP");
+            int val1;
+            int val2;
+            int val3;
+            int val4;
+
+            Console.WriteLine("Enter the start point");
+
+            Console.WriteLine("Enter x1: ");
+            val1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y1: ");
+            val2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the end point");
+
+            Console.WriteLine("Enter x2: ");
+            val3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y2: ");
+            val4 = Convert.ToInt32(Console.ReadLine());
+
+            double length = Math.Sqrt((val3 - val1) ^ 2 + (val4 - val2) ^ 2);
+            Console.WriteLine("Length of line is:" + length);
+        }
+        static void Main(string[] args)
+        {
+            Program lineLength = new Program();
+            lineLength.LineofLength();
         }
     }
+   
 }
 
  
